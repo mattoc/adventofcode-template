b8e566e3d6d7e6c4d5460e4e77020acc72895ff3
Matt Stevenson <mattoc@mattoc.com>
2023-12-03 03:08:30 +1100

    Ensure tests exist in correct module namespace (#5)

--- src/day1/solution.rb
+++ src/day1/solution.rb
@@ -2,27 +2,27 @@ require 'minitest/autorun'
 
 require_relative '../../lib/solution_base'
 
-class SolutionTest < Minitest::Test
-  make_my_diffs_pretty!
-
-  def setup
-    @solution = Day1::Solution.new
-  end
-
-  def test_sample
-    assert_output(/Loaded sample input:/) { @solution.sample }
-  end
-
-  def test_part_one
-    assert_output(/Loaded actual input:/) { @solution.part_one }
-  end
-
-  def test_part_two
-    skip 'Not implemented yet'
-  end
-end
-
 module Day1
+  class SolutionTest < Minitest::Test
+    make_my_diffs_pretty!
+
+    def setup
+      @solution = Day1::Solution.new
+    end
+
+    def test_sample
+      assert_output(/Loaded sample input:/) { @solution.sample }
+    end
+
+    def test_part_one
+      assert_output(/Loaded actual input:/) { @solution.part_one }
+    end
+
+    def test_part_two
+      skip 'Not implemented yet'
+    end
+  end
+
   class Solution < SolutionBase
     def sample
       puts 'Loaded sample input:'
